96872fdaa177706d3c4c2f04d222e5b02351bf04
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-12-03 16:34:03 -0500

    bump

--- crates/vox_core/src/lib.rs
+++ crates/vox_core/src/lib.rs
@@ -21,9 +21,48 @@ impl Ray
             dir
         }
     }
+
+    pub fn from_points<T>(origin: T, destination: T) -> Self
+        where T : Into<Vec3> + Copy
+    {
+        Self 
+        {
+            origin: origin.into(),
+            dir: origin.into() - destination.into()
+        }
+    }
+
+    pub fn from_points_normalized<T>(origin: T, destination: T) -> Self
+        where T : Into<Vec3> + Copy
+    {
+        let dir: Vec3 = origin.into() - destination.into();
+        Self 
+        {
+            origin: origin.into(),
+            dir: dir.normalize()
+        }
+    }
 }
 
 pub trait Intersectable 
 {
     fn intersect(&self, ray: &Ray) -> bool;
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct Camera
+{
+    pub eye: Vec3,
+    pub target: Vec3,
+    pub fov: f32,
+    pub aspect: f32
+}
+
+pub struct RTCameraInfo
+{
+    pub horizontal: Vec3,
+    pub vertical: Vec3,
+    pub width: u32,
+    pub height: u32,
 }
